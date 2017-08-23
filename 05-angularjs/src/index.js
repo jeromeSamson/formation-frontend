cba9dbd272a6f546b178be6689f067576107ecfd
Jérôme SAMSON DIT LAVIGNE <jerome.samson07@gmail.com>
2017-08-23 12:03:10 +0200

    tp02 angular vues et expressions

--- 05-angularjs/src/index.js
+++ 05-angularjs/src/index.js
@@ -1,5 +1,6 @@
 import tplTp01 from './tp01/expression.html'
+import tplTp02 from './tp02/vues.html'
 import anguler from 'angular'
-document.querySelector('body').innerHTML= [tplTp01].join('<hr>')
+document.querySelector('body').innerHTML= [tplTp01,tplTp02].join('<hr>')
 
 console.log('Bonjour App');
